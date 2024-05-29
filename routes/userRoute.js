a72e60cca802be12bb11b3c444592c132b10ec45
AkshandraSingh <nameste380@gmail.com>
2024-05-29 12:44:36 +0530

    Updated: addedUserValidations

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,14 +2,15 @@ const express = require('express')
 
 const userController = require('../controllers/userController')
 const { upload } = require('../middleware/userImageStorage')
+const userValidator = require('../validations/userValidations/userValidator')
 
 const userRouter = express.Router()
 
-userRouter.post('/registerUser', userController.registerUser)
-userRouter.post('/loginUser', userController.loginUser)
+userRouter.post('/registerUser', userValidator.registerUserValidation, userController.registerUser)
+userRouter.post('/loginUser', userValidator.loginUserValidation, userController.loginUser)
 userRouter.post('/forgetPassword', userController.forgetPassword)
-userRouter.post('/resetPassword/:userId/:token', userController.resetPassword)
-userRouter.post('/setNewPassword/:userId', userController.setNewPassword)
+userRouter.post('/resetPassword/:userId/:token', userValidator.resetPasswordValidation, userController.resetPassword)
+userRouter.post('/setNewPassword/:userId', userValidator.setNewPasswordValidation, userController.setNewPassword)
 userRouter.post('/changeProfilePic/:userId', upload.single('userProfilePic'), userController.changeProfilePic)
 
 module.exports = userRouter
